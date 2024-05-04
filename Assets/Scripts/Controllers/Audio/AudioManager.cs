875263d894dff7f21fd835f5ad40e6956eb44286
qqneifq <aidar777130@gmail.com>
2024-05-04 18:31:17 +0300

    1st

---
+++ Assets/Scripts/Controllers/Audio/AudioManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public SoundTrack[] tracks;
+
+
+    private void Awake()
+    {
+        foreach(SoundTrack track in tracks)
+        {
+            track.source = gameObject.AddComponent<AudioSource>();
+            track.source.clip = track.clip;
+            track.source.volume = track.volume;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Play("BGM");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play(string name)
+    {
+        SoundTrack t = Array.Find(tracks, track => track.name == name);
+        t.source.loop = true;
+        t.source.Play();
+    }
+}
