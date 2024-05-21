a711d693355a31f189f54e44a803248336004985
Hadukin-Nik <68010131+Hadukin-Nik@users.noreply.github.com>
2024-05-21 13:26:09 +0300

    'asd'

--- Assets/Scripts/Models/BuildingsConsts.cs
+++
@@ -1,7 +0,0 @@
-public class BuildingsConsts
-{
-    public enum BuildingIndificator
-    {
-        Desk1, Desk2, Desk3
-    }
-}
--- Assets/Scripts/Models/ItemsConsts.cs
+++
@@ -1,7 +0,0 @@
-public class ItemsConsts
-{
-    public enum ItemIndificator
-    {
-        Bone, Meat, Sword, Bowl, Hat, Empty
-    }
-}
